90efeec38bc8a3f177b87301ef116c49723b410a
Danilo Caruso <danilocaruso87@gmail.com>
2022-10-13 22:43:36 +0200

    upload db test agenda appuntamenti

---
+++ agenda.sql
@@ -0,0 +1,68 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.0
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Creato il: Ott 13, 2022 alle 22:42
+-- Versione del server: 8.0.29
+-- Versione PHP: 7.4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `agenda`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Struttura della tabella `appuntamenti`
+--
+
+CREATE TABLE `appuntamenti` (
+  `id` int NOT NULL,
+  `str_data` int DEFAULT NULL,
+  `titolo` varchar(45) DEFAULT NULL,
+  `testo` varchar(45) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+--
+-- Dump dei dati per la tabella `appuntamenti`
+--
+
+INSERT INTO `appuntamenti` (`id`, `str_data`, `titolo`, `testo`) VALUES
+(2, 1665612000, 'appuntamento 1', 'aaaa'),
+(3, 1665266400, 'appuntamento 2', 'rrr');
+
+--
+-- Indici per le tabelle scaricate
+--
+
+--
+-- Indici per le tabelle `appuntamenti`
+--
+ALTER TABLE `appuntamenti`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT per le tabelle scaricate
+--
+
+--
+-- AUTO_INCREMENT per la tabella `appuntamenti`
+--
+ALTER TABLE `appuntamenti`
+  MODIFY `id` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
