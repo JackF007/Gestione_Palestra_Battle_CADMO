12d385e9d42631d1643d9eeeeb7846b1ee630c27
Danilo Caruso <danilocaruso87@gmail.com>
2022-10-23 08:58:23 +0200

    add dash button logout

--- dashboard.php
+++ dashboard.php
@@ -11,7 +11,7 @@ $session_ruolo = htmlspecialchars($_SESSION['session_ruolo'], ENT_QUOTES, 'UTF-8
 
 if (!(isset($_SESSION['session_id']))) {
     header("location:login.php");
-} else if ( "amministrazione" != $session_ruolo){
+} else if ("amministrazione" != $session_ruolo) {
     header("location:profile.php");
 }
 
@@ -58,31 +58,8 @@ if (!(isset($_SESSION['session_id']))) {
             <!-- Right navbar links -->
             <ul class="navbar-nav ml-auto">
                 <!-- Notifications Dropdown Menu -->
-                <li class="nav-item dropdown">
-                    <a class="nav-link" data-toggle="dropdown" href="#">
-                        <i class="far fa-bell"></i>
-                        <span class="badge badge-warning navbar-badge">15</span>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-lg dropdown-menu-right">
-                        <span class="dropdown-item dropdown-header">15 Notifications</span>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-envelope mr-2"></i> 4 new messages
-                            <span class="float-right text-muted text-sm">3 mins</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-users mr-2"></i> 8 friend requests
-                            <span class="float-right text-muted text-sm">12 hours</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item">
-                            <i class="fas fa-file mr-2"></i> 3 new reports
-                            <span class="float-right text-muted text-sm">2 days</span>
-                        </a>
-                        <div class="dropdown-divider"></div>
-                        <a href="#" class="dropdown-item dropdown-footer">See All Notifications</a>
-                    </div>
+                <li class="nav-item">
+                    <button type="button" onclick=logout() class="btn btn-outline-primary btn-block"><i class="fa fa-arrow-left"></i> Logout</button>
                 </li>
                 <li class="nav-item">
                     <a class="nav-link" data-widget="fullscreen" href="#" role="button">
@@ -405,7 +382,7 @@ if (!(isset($_SESSION['session_id']))) {
                                                 <th style="width: 10%" class="text-center">
                                                     Status
                                                 </th>
-                                                
+
                                             </tr>
                                         </thead>
                                         <tbody>
@@ -498,8 +475,8 @@ if (!(isset($_SESSION['session_id']))) {
     } else if (isset($_REQUEST["nonprenotato"])) {
         echo "<script>notSuccessPrenozazione();</script>";
     }
-    
-    
+
+
     ?>
 </body>
 
